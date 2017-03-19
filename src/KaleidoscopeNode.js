1330008ff6cb9c164de3355c401390155256340b
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2017-03-19 02:06:20 +0100

    Add kaleidoscope node

--- src/KaleidoscopeNode.js
+++ src/KaleidoscopeNode.js
@@ -2,7 +2,17 @@
   class KaleidoscopeNode extends NIN.ShaderNode {
     constructor(id, options) {
       options.shader = SHADERS.kaleidoscope;
+      options.inputs = {
+        tDiffuse: new NIN.TextureInput()
+      };
+
       super(id, options);
+    }
+
+    update(frame) {
+      super.update(frame);
+
+      this.quad.material.uniforms.tDiffuse.value = this.inputs.tDiffuse.getValue();
     }
   }
 
