a944351aaaf3ab0df6a8ce43ec0e911caa65a61c
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2017-03-25 23:09:27 +0100

    Smoothstep the shader
    Makes it look better.

--- src/shaders/triangle/fragment.glsl
+++ src/shaders/triangle/fragment.glsl
@@ -4,12 +4,12 @@ uniform sampler2D tDiffuse;
 varying vec2 vUv;
 
 void main() {
-    vec4 color = vec4(1.0, 1.0, 1.0, 1.0);
+    float c = 1.0;
+    float dX = min(0.5, max(0.0, sin(frame / 80.0)));
+    float dY = min(0.5, max(0.0, cos(frame / 80.0)));
 
-    if(vUv.x <= min(0.5, max(0.0, sin(frame / 80.0))) ||
-       vUv.y <= min(0.5, max(0.0, cos(frame / 80.0)))) {
-        color.xyz = vec3(0.0, 0.0, 0.0);
-    }
+    c = smoothstep(dX, dX-0.001, vUv.x);
+    c += smoothstep(dY, dY-0.001, vUv.y);
 
-    gl_FragColor = color;
+    gl_FragColor = vec4(vec3(c), 1.0);
 }
