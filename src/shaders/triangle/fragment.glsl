c75e0f2000e931ab2632e8e772fa77051bbe60bb
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2017-03-26 01:34:58 +0100

    Add bubbleeffect after initial triangle

--- src/shaders/triangle/fragment.glsl
+++ src/shaders/triangle/fragment.glsl
@@ -2,6 +2,7 @@ uniform float frame;
 uniform sampler2D tDiffuse;
 
 varying vec2 vUv;
+varying vec2 aUv;
 
 void main() {
     float c = 1.0;
@@ -10,6 +11,15 @@ void main() {
 
     c = smoothstep(dX, dX-0.001, vUv.x);
     c += smoothstep(dY, dY-0.001, vUv.y);
+
+    if(frame > 5780.0) {
+        float r = 0.3*abs(sin(frame/80.0));
+        vec2 uv = aUv;
+        uv -= .5;
+        float d = length(vUv-.5);
+
+        c = smoothstep(r, r-0.01, d);
+    }
 
     gl_FragColor = vec4(vec3(c), 1.0);
 }
--- src/shaders/triangle/vertex.glsl
+++ src/shaders/triangle/vertex.glsl
@@ -1,8 +1,12 @@
+attribute vec2 aUV;
+
 uniform sampler2D tDiffuse;
 
 varying vec2 vUv;
+varying vec2 aUv;
 
 void main() {
     vUv = uv;
+    aUv = aUV;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
