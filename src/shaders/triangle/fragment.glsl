6feef329e13ff879b94d71dbdfc24313fd288eb7
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2017-04-03 20:41:17 +0200

    Change triangle to core effect

--- src/shaders/triangle/fragment.glsl
+++ src/shaders/triangle/fragment.glsl
@@ -6,20 +6,13 @@ varying vec2 aUv;
 
 void main() {
     float c = 1.0;
-    float dX = min(0.5, max(0.0, sin(frame / 80.0)));
-    float dY = min(0.5, max(0.0, cos(frame / 80.0)));
 
-    c = smoothstep(dX, dX-0.001, vUv.x);
-    c += smoothstep(dY, dY-0.001, vUv.y);
+    float r = 0.3*abs(sin(frame/80.0));
+    vec2 uv = aUv;
+    uv -= .5;
+    float d = length(vUv-.5);
 
-    if(frame > 5780.0) {
-        float r = 0.3*abs(sin(frame/80.0));
-        vec2 uv = aUv;
-        uv -= .5;
-        float d = length(vUv-.5);
-
-        c = smoothstep(r, r-0.01, d);
-    }
+    c = smoothstep(r, r-0.01, d);
 
     gl_FragColor = vec4(vec3(c), 1.0);
 }
