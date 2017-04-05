e4b05ec5b350c2f9d5e3aca6f00e58068f36a654
Rune Holmgren <Raane.Holm@gmail.com>
2017-04-05 20:54:35 +0200

    Add a bunch of unpolished stuff

--- src/cubeTunnel.js
+++ src/cubeTunnel.js
@@ -15,14 +15,15 @@
         format: THREE.RGBFormat
       });
       this.camera = new THREE.PerspectiveCamera(45, 16 / 9, 1, 10000);
-      this.camera.position.z = 100;
 
       this.top_material = new THREE.ShaderMaterial(SHADERS.topshader);
+      this.wall_material = new THREE.ShaderMaterial(SHADERS.wallshader);
 
       // Things used when spawning the first ring of cubes.
       this.spawningCubes = [];
+      this.box_geom = new THREE.BoxGeometry(8, 8, 0);
       for(var i=0; i<12; i++) {
-        var newCube = new THREE.Mesh(new THREE.BoxGeometry(8, 8, 0),
+        var newCube = new THREE.Mesh(this.box_geom,
                                      this.top_material);
         this.spawningCubes.push(newCube);
         this.spawningCubes[i].position.x = 1000;
@@ -30,7 +31,16 @@
       }
 
 
+        this.create_geoms();
 
+        this.create_layer(0);
+        this.create_layer(-20);
+        this.create_layer(-40);
+        this.create_layer(-60);
+        this.create_layer(-80);
+        this.create_layer(-100);
+        this.create_layer(-120);
+        this.create_layer(-140);
     }
 
     update(frame) {
@@ -40,6 +50,7 @@
 
       if (BEAN < 3936) {
         // Spawning the first "ring" of cubes.
+        this.camera.position.z = 100;
         var slideDuration = 12;
         var cornerSlideDuration = 24;
 
@@ -96,7 +107,10 @@
         this.spawningCubes[11].position.y = -12;
       } else {
         // Spawning the rest of the scene and start playing with camera.
+        this.camera.position.z = 0;
+        this.camera.position.y = 34;
 
+        this.camera.lookAt(new THREE.Vector3(0,0,0));
       }
 
       // When BEAN is equal to startBEAN + one of the numbers in the list, a ring is spawned on the texture on top of the cubes.
@@ -128,7 +142,9 @@
       this.top_material.uniforms.start2.value = clamp(0, stripe_position2 - 0.2, 1);
       this.top_material.uniforms.stop2.value = clamp(0, stripe_position2, 1);
       // Set the number tiles in the shader. Can be set to more or less anything, but small numbers are best IMO.
-      this.top_material.uniforms.tiles.value = 1;
+      this.top_material.uniforms.tiles.value = 4;
+
+      this.wall_material.uniforms.time.value = Math.floor((BEAN / 6) % 4) * 100;
     }
 
     render(renderer) {
@@ -139,6 +155,236 @@
     resize() {
       this.renderTarget.setSize(16 * GU, 9 * GU);
      }
+
+
+
+
+
+
+
+
+
+
+
+// Create a full layer of squares
+    create_layer(y) {
+      var distance = 8.485; // sqrt(6^2 + 6^2)
+      var elevation = 1;
+      var elevation2 = 0.5;
+      var elevation3 = 1;
+
+      // Center cube
+      this.create_small(0,y+elevation,0,0);
+      this.create_large(0,y,0,Math.PI/4);
+
+      // Over, under, right left cube
+      this.create_small(distance,y+elevation,0,0);
+      this.create_large(distance,y+elevation2,0,Math.PI/4);
+
+      this.create_small(-distance,y+elevation,0,0);
+      this.create_large(-distance,y+elevation2,0,Math.PI/4);
+
+      this.create_small(0,y+elevation,distance,0);
+      this.create_large(0,y+elevation2,distance,Math.PI/4);
+
+      this.create_small(0,y+elevation,-distance,0);
+      this.create_large(0,y+elevation2,-distance,Math.PI/4);
+
+      // Corner cubes
+      this.create_small(distance,y+elevation,distance,0);
+      this.create_large(distance,y+elevation3,distance,Math.PI/4);
+
+      this.create_small(-distance,y+elevation,-distance,0);
+      this.create_large(-distance,y+elevation3,-distance,Math.PI/4);
+
+      this.create_small(distance,y+elevation,-distance,0);
+      this.create_large(distance,y+elevation3,-distance,Math.PI/4);
+
+      this.create_small(-distance,y+elevation,distance,0);
+      this.create_large(-distance,y+elevation3,distance,Math.PI/4);
+    }
+
+    // Create a small cube at the desired location.
+    create_small(x, y, z, ry) {
+      var mesh = new THREE.Mesh();
+
+      this.create_top(mesh, this.small_square_geom, this.top_material);
+
+      this.create_walls(mesh, 1, this.wall_material, false);
+      this.create_walls(mesh, 2, this.wall_material, true);
+
+      mesh.position.x = x;
+      mesh.position.y = y;
+      mesh.position.z = z;
+      mesh.rotation.y = ry;
+
+      var small_scale = 1.2;
+      mesh.scale.set(small_scale, small_scale, small_scale);
+
+      this.scene.add(mesh);
+    }
+
+    // Create a large cube at the desired location.
+    create_large(x, y, z, ry) {
+      var mesh = new THREE.Mesh();
+
+      this.create_top(mesh, this.large_square_geom, this.top_material);
+
+      this.create_walls(mesh, 3, this.wall_material, false);
+       this.create_walls(mesh, 4, this.wall_material, true);
+
+      mesh.position.x = x;
+      mesh.position.y = y;
+      mesh.position.z = z;
+      mesh.rotation.y = ry;
+
+      this.scene.add(mesh);
+    }
+
+    // Create a top
+    create_top(parrent, geometry, material) {
+      var mesh = new THREE.Mesh( geometry, material );
+      parrent.add( mesh );
+    }
+
+    // Create walls with an offset from the center forming a square tube facing inward or outward.
+    create_walls(parrent, offset, material, outer_wall) {
+
+      var small_inner_side_geom = new THREE.PlaneGeometry(2 * offset, 2 * offset);
+
+      var side1 = new THREE.Mesh( small_inner_side_geom, material );
+      side1.position.set(0, -offset, -offset);
+      side1.rotation.y = Math.PI * outer_wall;
+      parrent.add( side1 );
+
+      var side2 = new THREE.Mesh( small_inner_side_geom, material );
+      side2.position.set(offset, -offset, 0);
+      side2.rotation.y = Math.PI * -0.5 + Math.PI * outer_wall;
+      parrent.add( side2 );
+
+      var side3 = new THREE.Mesh( small_inner_side_geom, material );
+      side3.position.set(0, -offset, offset);
+      side3.rotation.y = Math.PI * 1 + Math.PI * outer_wall;
+      parrent.add( side3 );
+
+      var side4 = new THREE.Mesh( small_inner_side_geom, material );
+      side4.position.set(-offset, -offset, 0);
+      side4.rotation.y = Math.PI * 0.5 + Math.PI * outer_wall;
+      parrent.add( side4 );
+    }
+
+    // Create the custom geometry for the top surfaces (square with hole)
+    create_geoms() {
+      // Large square with hole
+      this.large_square_geom = new THREE.Geometry(); 
+      var lv0 = new THREE.Vector3(4,0,4);
+      var lv1 = new THREE.Vector3(3,0,3);
+      var lv2 = new THREE.Vector3(4,0,-4);
+      var lv3 = new THREE.Vector3(3,0,-3);
+      var lv4 = new THREE.Vector3(-4,0,4);
+      var lv5 = new THREE.Vector3(-3,0,3);
+      var lv6 = new THREE.Vector3(-4,0,-4);
+      var lv7 = new THREE.Vector3(-3,0,-3);
+
+      this.large_square_geom.vertices.push(lv0);
+      this.large_square_geom.vertices.push(lv1);
+      this.large_square_geom.vertices.push(lv2);
+      this.large_square_geom.vertices.push(lv3);
+      this.large_square_geom.vertices.push(lv4);
+      this.large_square_geom.vertices.push(lv5);
+      this.large_square_geom.vertices.push(lv6);
+      this.large_square_geom.vertices.push(lv7);
+
+      this.large_square_geom.faces.push( new THREE.Face3( 0, 2, 1 ) );
+      this.large_square_geom.faces.push( new THREE.Face3( 1, 2, 3 ) );
+      this.large_square_geom.faces.push( new THREE.Face3( 0, 1, 5 ) );
+      this.large_square_geom.faces.push( new THREE.Face3( 0, 5, 4 ) );
+      this.large_square_geom.faces.push( new THREE.Face3( 4, 5, 7 ) );
+      this.large_square_geom.faces.push( new THREE.Face3( 4, 7, 6 ) );
+      this.large_square_geom.faces.push( new THREE.Face3( 3, 2, 6 ) );
+      this.large_square_geom.faces.push( new THREE.Face3( 3, 6, 7 ) );
+
+      var lt0 = new THREE.Vector2( 1, 1 );
+      var lt1 = new THREE.Vector2( 7/8, 7/8 );
+      var lt2 = new THREE.Vector2( 1, 0 );
+      var lt3 = new THREE.Vector2( 7/8, 1/8 );
+      var lt4 = new THREE.Vector2( 0, 1 );
+      var lt5 = new THREE.Vector2( 1/8, 7/8 );
+      var lt6 = new THREE.Vector2( 0, 0 );
+      var lt7 = new THREE.Vector2( 1/8, 1/8 );
+
+      this.large_square_geom.faceVertexUvs[0][0] = [lt0, lt2, lt1];
+      this.large_square_geom.faceVertexUvs[0][1] = [lt1, lt2, lt3];
+      this.large_square_geom.faceVertexUvs[0][2] = [lt0, lt1, lt5];
+      this.large_square_geom.faceVertexUvs[0][3] = [lt0, lt5, lt4];
+      this.large_square_geom.faceVertexUvs[0][4] = [lt4, lt5, lt7];
+      this.large_square_geom.faceVertexUvs[0][5] = [lt4, lt7, lt6];
+      this.large_square_geom.faceVertexUvs[0][6] = [lt3, lt2, lt6];
+      this.large_square_geom.faceVertexUvs[0][7] = [lt3, lt6, lt7];
+
+    // Small square with hole
+      this.small_square_geom = new THREE.Geometry(); 
+      var sv0 = new THREE.Vector3( 2, 0, 2 );
+      var sv1 = new THREE.Vector3( 1, 0, 1 );
+      var sv2 = new THREE.Vector3( 2, 0, -2 );
+      var sv3 = new THREE.Vector3( 1, 0, -1 );
+      var sv4 = new THREE.Vector3( -2, 0, 2 );
+      var sv5 = new THREE.Vector3( -1, 0, 1 );
+      var sv6 = new THREE.Vector3( -2, 0, -2 );
+      var sv7 = new THREE.Vector3( -1, 0, -1 );
+
+      this.small_square_geom.vertices.push( sv0 );
+      this.small_square_geom.vertices.push( sv1 );
+      this.small_square_geom.vertices.push( sv2 );
+      this.small_square_geom.vertices.push( sv3 );
+      this.small_square_geom.vertices.push( sv4 );
+      this.small_square_geom.vertices.push( sv5 );
+      this.small_square_geom.vertices.push( sv6 );
+      this.small_square_geom.vertices.push( sv7 );
+
+      this.small_square_geom.faces.push( new THREE.Face3( 0, 2, 1 ) );
+      this.small_square_geom.faces.push( new THREE.Face3( 1, 2, 3 ) );
+      this.small_square_geom.faces.push( new THREE.Face3( 0, 1, 5 ) );
+      this.small_square_geom.faces.push( new THREE.Face3( 0, 5, 4 ) );
+      this.small_square_geom.faces.push( new THREE.Face3( 4, 5, 7 ) );
+      this.small_square_geom.faces.push( new THREE.Face3( 4, 7, 6 ) );
+      this.small_square_geom.faces.push( new THREE.Face3( 3, 2, 6 ) );
+      this.small_square_geom.faces.push( new THREE.Face3( 3, 6, 7 ) );
+
+      var st0 = new THREE.Vector2( 1, 1 );
+      var st1 = new THREE.Vector2( 3/4, 3/4 );
+      var st2 = new THREE.Vector2( 1, 0 );
+      var st3 = new THREE.Vector2( 3/4, 1/4 );
+      var st4 = new THREE.Vector2( 0, 1 );
+      var st5 = new THREE.Vector2( 1/4, 3/4 );
+      var st6 = new THREE.Vector2( 0, 0 );
+      var st7 = new THREE.Vector2( 1/4, 1/4 );
+
+      this.small_square_geom.faceVertexUvs[0][0] = [st0, st2, st1];
+      this.small_square_geom.faceVertexUvs[0][1] = [st1, st2, st3];
+      this.small_square_geom.faceVertexUvs[0][2] = [st0, st1, st5];
+      this.small_square_geom.faceVertexUvs[0][3] = [st0, st5, st4];
+      this.small_square_geom.faceVertexUvs[0][4] = [st4, st5, st7];
+      this.small_square_geom.faceVertexUvs[0][5] = [st4, st7, st6];
+      this.small_square_geom.faceVertexUvs[0][6] = [st3, st2, st6];
+      this.small_square_geom.faceVertexUvs[0][7] = [st3, st6, st7];
+
+      // Planes
+      this.small_outer_side_geom = new THREE.PlaneGeometry( 4, 4 );
+      this.large_inner_side_geom = new THREE.PlaneGeometry( 6, 6 );
+      this.large_outer_side_geom = new THREE.PlaneGeometry( 8, 8 );
+
+    };
+
+
+
+
+
+
+
+
+
+
   }
 
   global.cubeTunnel = cubeTunnel;
