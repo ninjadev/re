1f03a03ff52f5ad7068168c710a96beb7b6f8a32
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2017-04-09 20:57:32 +0200

    Add blur to sphereNode

--- src/AddNode.js
+++ src/AddNode.js
@@ -10,9 +10,18 @@
     }
 
     update(frame) {
+      if (typeof this.opacity == 'number') {
+        this.uniforms.opacity.value = this.opacity;
+      } else {
+        if (frame >= this.opacity.from && frame < this.opacity.to) {
+          this.uniforms.opacity.value = this.opacity.value;
+        } else {
+          this.uniforms.opacity.value = this.opacity.default;
+        }
+      }
+
       this.uniforms.A.value = this.inputs.A.getValue();
       this.uniforms.B.value = this.inputs.B.getValue();
-      this.uniforms.opacity.value = this.opacity;
     }
   }
 
