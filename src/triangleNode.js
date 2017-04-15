9fcf07fc9a3e963abac111236a9944dc5de6abf7
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2017-04-15 23:05:00 +0200

    Remove blob

--- src/triangleNode.js
+++ src/triangleNode.js
@@ -7,7 +7,7 @@
       };
       super(id, options);
 
-      this.analysis = new audioAnalysisSanitizer('stem_snare.wav', 'spectral_energy', 0.16);
+      this.uniforms.big.value = 0;
     }
     update(frame) {
       if(BEAN < 12 * 4 * 49) {
@@ -19,41 +19,6 @@
         this.inputs.B.enabled = true;
         this.uniforms.A.value = this.inputs.B.getValue();
       }
-
-      let frameOffset = 0;
-      if(BEAN > 12 * 4 * 51) {
-        frameOffset = this.analysis.getValue(frame) * 32 * (BEAN >= 12 * 4 * 49) * (BEAN % 12 == 0 ? 1 : -1);
-        if(BEAN < 12 * 4 * 54) {
-          frameOffset = this.analysis.getValue(frame) * 87 * (BEAN >= 12 * 4 * 49) * (BEAN % 12 == 0 ? 1 : -0.8) + Math.sin(frame/1000);
-        }
-      }
-
-      this.uniforms.big.value = lerp(0, Math.max(Math.sin(frame/100), 0.3), (frame - FRAME_FOR_BEAN(12 * 4 * 49)) / 100);
-      this.uniforms.frame.value = frame * 2 - frameOffset;
-
-      this.uniforms.amount.value = lerp(0, 1., (frame - FRAME_FOR_BEAN(12 * 4 * 48.5)) / 50);
-
-      if(BEAN >= 12 * 4 * 53 - 12) {
-        if(BEAN < 12 * 4 * 53 - 8) {
-          this.uniforms.frame.value = 0;
-        } else if(BEAN < 12 * 4 * 53 - 6) {
-          this.uniforms.big.value = 0.3;
-        } else if(BEAN < 12 * 4 * 53 - 3) {
-          this.uniforms.frame.value = 600;
-        } else if(BEAN < 12 * 4 * 53) {
-          this.uniforms.frame.value = 300;
-        }
-      }
-
-      if (frame > 6202) {
-        if (frame < 6257.) {
-          this.uniforms.frame.value = 1337;
-        } else if (frame < 6278.) {
-          this.uniforms.frame.value = -1133;
-        } else if (frame < 6299.) {
-          this.uniforms.frame.value = 1337;
-        }
-      } 
     }
   }
 
