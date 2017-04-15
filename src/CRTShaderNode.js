ea481309e2373d0053909a992806373b6540a0ba
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-04-15 02:38:21 +0200

    Add more maximalistic minecraft-to-ifs transition

--- src/CRTShaderNode.js
+++ src/CRTShaderNode.js
@@ -2,14 +2,25 @@
   class CRTShaderNode extends NIN.ShaderNode {
     constructor(id, options) {
       options.inputs = {
-        tDiffuse: new NIN.TextureInput()
+        tDiffuse: new NIN.TextureInput(),
+        tDiffuse2: new NIN.TextureInput()
       };
       super(id, options);
     }
 
     update(frame) {
       this.uniforms.frame.value = frame;
-      this.uniforms.tDiffuse.value = this.inputs.tDiffuse.getValue();
+      if(BEAN >= 12 * 4 * 84) {
+        this.inputs.tDiffuse.node.active = false;
+        this.inputs.tDiffuse2.node.active = true;
+        this.uniforms.tDiffuse.value = this.inputs.tDiffuse.getValue();
+      } else {
+        this.inputs.tDiffuse.node.active = true;
+        this.inputs.tDiffuse2.node.active = false;
+        this.uniforms.tDiffuse.value = this.inputs.tDiffuse2.getValue();
+      }
+      this.uniforms.amount.value = smoothstep(0, 1.5, (frame - FRAME_FOR_BEAN(12 * 4 * 83.5)) / 50);
+      this.uniforms.amount.value = smoothstep(this.uniforms.amount.value, 1, (frame - FRAME_FOR_BEAN(12 * 4 * 83.75)) / 30);
     }
   }
 
