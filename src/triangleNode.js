5991772c75857fa0b8a486acc9bbc3a6e05c8d9f
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-04-16 03:53:02 +0200

    Add backup effect in case we need it

--- src/triangleNode.js
+++ src/triangleNode.js
@@ -7,9 +7,19 @@
       };
       super(id, options);
 
-      this.uniforms.big.value = 0;
+      this.throb = 0;
     }
+
     update(frame) {
+      this.uniforms.frame.value = frame;
+
+      this.throb *= 0.95;
+      if(BEAT && BEAN % 12 == 0) {
+        this.throb = 1;
+      }
+
+      this.uniforms.disformationAmount.value = this.throb;
+
       if(BEAN < 12 * 4 * 49) {
         this.inputs.A.enabled = true;
         this.inputs.B.enabled = false;
@@ -18,6 +28,25 @@
         this.inputs.A.enabled = false;
         this.inputs.B.enabled = true;
         this.uniforms.A.value = this.inputs.B.getValue();
+      }
+
+      if(BEAN < 12 * 4 * 49) {
+        this.uniforms.bounceSpeedMultiplier.value = 0.25 / 2;
+      } else if(BEAN < 12 * 4 * (49 + 4 - 0.5)) {
+        this.uniforms.bounceSpeedMultiplier.value = 0.25;
+      } else if(BEAN < 12 * 4 * (49 + 4)) {
+        this.uniforms.bounceSpeedMultiplier.value = 0.5;
+      } else if(BEAN < 12 * 4 * (49 + 11)) {
+        this.uniforms.bounceSpeedMultiplier.value = 0.5;
+      }
+    }
+
+    render(renderer) {
+      super.render(renderer);
+      if(BEAN < 12 * 4 * 49) {
+        this.outputs.render.setValue(this.inputs.A.getValue());
+      } else {
+        this.outputs.render.setValue(this.renderTarget.texture);
       }
     }
   }
