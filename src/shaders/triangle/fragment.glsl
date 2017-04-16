99cb45073e9d9c98fb7f30c875ac55fc4aa93e7c
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-04-16 03:53:02 +0200

    Add metaball effect to jules scene
    Basing the effect off of @aleksanb's ray marcher.

--- src/shaders/triangle/fragment.glsl
+++ src/shaders/triangle/fragment.glsl
@@ -1,102 +1,116 @@
 uniform float frame;
+uniform float bounceSpeedMultiplier;
+uniform float disformationAmount;
 uniform sampler2D A;
-uniform vec2 resolution;
-uniform float amount;
-
-uniform float big;
-uniform float extra;
-uniform float shift;
+#define PI 3.141592653589793
 
 varying vec2 vUv;
 
-#define EPS 0.01
-#define FAR 10.0
-#define STEPS 64
+float sphere(vec3 position, vec3 sphere, float radius) {
+    return length(position - sphere) - radius;
+}
 
+float box(vec3 p, vec3 b) {
+    vec3 d = abs(p) - b;
+    return min(max(d.x,max(d.y,d.z)),0.0) + length(max(d,0.0));
+}
+
+
+float torus(vec3 p, vec2 t) {
+    vec2 q = vec2(length(p.xz) - t.x, p.y);
+    return length(q) - t.y;
+}
+
+float displacement(vec3 p) {
+    return 1.5 * sin(frame / 9. + .9 * p.x) * sin(.5 * p.y) * sin(.9 * p.z);
+}
+
+float cylinder( vec3 p, vec2 h ) {
+    vec2 d = abs(vec2(length(p.xz),p.y)) - h;
+    return min(max(d.x,d.y),0.0) + length(max(d,0.0));
+}
+
+
+float smin(float a, float b, float k) {
+    float res = exp(-k * a) + exp(-k * b);
+    return -log(res) / k;
+}
 
 float map(vec3 p) {
-    float v = frame / 60.;
-    float r = max((0.9*big + extra), 0.5) +
-               0.35*cos(8.3*p.y + v) +
-               0.35*cos(2.3*p.x + v);
-    return length(p) - r;
+    float timer = 2. * PI * frame / 60. / 60. * 130. * bounceSpeedMultiplier;
+    float s = sphere(p, vec3(2. * sin(timer / 2.), -5. * sin(timer), 2. * sin(timer)), 4.);
+    s = smin(s, sphere(p, vec3(-2. * sin(timer / 2.), 5. * sin(timer), .0), 4.), .5);
+    float t = torus(p, vec2(5., 1.));
+    s += disformationAmount * displacement(p);
+    return s;
 }
 
-float map_black(vec3 p) {
-    return map(p) - 0.05;
+float castRay(vec3 ro, vec3 rd, float sizeChange) {
+    float totalDistance = 0.0;
+    const int maxSteps = 128;
+
+    for(int i = 0; i < maxSteps; ++i) {
+        vec3 p = ro + rd * totalDistance;
+        float d = map(p*(1. + sizeChange));
+        if(d < 0.001 || totalDistance >= 40.0) {
+            break;
+        }
+
+        totalDistance += d;
+    }
+    return totalDistance;
 }
 
-vec3 calcNormal(in vec3 pos) {
-    vec2 e = vec2(1.0,-1.0)*0.5773*0.0005;
-    return normalize(
-        e.xyy*map(pos + e.xyy) +
-        e.yyx*map(pos + e.yyx) +
-        e.yxy*map(pos + e.yxy) +
-        e.xxx*map(pos + e.xxx)
-    );
-}
-
-vec3 shade(vec4 color, vec2 uv, vec3 rd, vec3 ro, float multiplier) {
-    float l = length(uv);
-    float t = frame / 60.;
-
-    vec3 norm = calcNormal(rd);
-    vec3 refd = refract(rd, norm, 1.0/1.33);
-
-    vec3 light = vec3(1.0, 1.0, -1.0);
-    float diffusion = clamp(dot(norm, light), 0.0, 1.0);
-
-    vec3 cam = ro+rd*multiplier;
-    vec2 point = vec2(refd.x + cam.x, refd.y + cam.y);
-    vec4 col = texture2D(A, mod(0.35*point+.5,1.));
-
-    col = vec4(.3*diffusion) + .7*col;
-
-    col = max(vec4(0.15), col);
-
-    // post processing
-    col = smoothstep(0.0, 1.0, col);
-    vec3 res = pow(col.xyz, vec3(0.45));
-    return col.xyz;
+vec3 calculateNormal(vec3 pos) {
+    vec3 eps = vec3(0.001, 0.0, 0.0);
+    vec3 normal = vec3(
+            map(pos + eps.xyy) - map(pos - eps.xyy),
+            map(pos + eps.yxy) - map(pos - eps.yxy),
+            map(pos + eps.yyx) - map(pos - eps.yyx));
+    return normalize(normal);
 }
 
 void main() {
-    vec4 bg = texture2D(A, vUv);
+    float x = (vUv.x * 16.0) - 8.0;
+    float y = (vUv.y * 9.0) - 4.5;
+    float fov = 18.0;
 
-    vec2 uv = -1.0 + 2.0 * vUv;
-    uv.x *= 16.0/9.0;
-    vec3 ro = vec3(0.0, 0.0, 2.5);
-    vec3 rd = normalize(vec3(uv, -1.0));
+    vec3 eye = vec3(0., 0., 50.);
+    vec3 forward = vec3(0., 0., -1.);
+    vec3 up = vec3(0.0, 1.0, 0.0);
+    vec3 right = cross(forward, up);
 
-    float t = 0.0;
-    float d = EPS;
+    vec3 rayOrigin = eye + (right * x) + (up * y) + (forward * fov);
+    vec3 rayDestination = normalize(rayOrigin - eye);
 
-    // background
-    vec3 c = mix(vec3(0.0), bg.xyz, 2.0 - length(uv));
+    vec3 light = normalize(vec3(1., 1., 1.));
 
-    // black outline
-    for(int i = 0; i < STEPS; ++i) {
-        d = map_black(ro + t*rd);
-        if(d < EPS || t > FAR) break;
-        t += d;
+    float farClippingPlane = 40.0;
+
+    float blackColDistance = castRay(rayOrigin, rayDestination, -0.03);;
+    vec4 blackCol = vec4(0.);
+
+    float totalDistance = castRay(rayOrigin, rayDestination, 0.0);
+    vec4 color = vec4(1.);
+    vec3 pos = rayOrigin + forward * totalDistance;
+    vec3 surfaceNormal = calculateNormal(pos);
+    float diffusion = 1.5 * clamp(dot(surfaceNormal, light), 0.0, 1.0);
+
+    vec3 refracted = refract(normalize(pos - eye), surfaceNormal, 1.33);
+    vec4 refractedColor = texture2D(A, mod(refracted.xy - 0.5, 1.));
+
+    if(blackColDistance < farClippingPlane) {
+        color = vec4(.1, 0., .1, 1.);
     }
 
-    if(d < EPS) {
-        c = vec3(0.0);
+    if(totalDistance < farClippingPlane) {
+        color = diffusion * vec4(0.4, 0.1, 0.4, 1.) * .8 + refractedColor * 0.4;
+    }
+    //color = vec4(surfaceNormal, 1.);
+
+    if(blackColDistance > farClippingPlane) {
+        color = texture2D(A, vUv);
     }
 
-    // blob
-    t = 0.0;
-    d = EPS;
-    for(int i = 0; i < STEPS; ++i) {
-        d = map(ro + t*rd);
-        if(d < EPS || t > FAR) break;
-        t += d;
-    }
-
-    if(d < EPS) {
-        c = shade(bg, vUv, rd, ro, t);
-    }
-
-    gl_FragColor = mix(bg, vec4(c, 1.0), amount);
+    gl_FragColor = color;
 }
